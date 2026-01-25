9c5797d848b7caa60219cc335888e95ee53285b4
sagarkanungo <140177029+sagarkanungo@users.noreply.github.com>
2026-01-26 00:50:16 +0530

    Update server port configuration

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ setInterval(() => {
   });
 }, 5 * 60 * 1000);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
