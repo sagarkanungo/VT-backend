bfc00600891671f2ec9b64dbe4eda5c0ac4e5cf1
Sagar Kanungo <sagarkanungo23@gmail.com>
2026-01-26 12:47:49 +0530

    refactore code14

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.use("/api", require("./routes/analytics.routes"));
 // ========================
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
